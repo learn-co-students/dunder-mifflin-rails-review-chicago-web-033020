a74f46aebbc394ffbcf352a44083078dc833eafe
Joanna Lin <lin.joanna.y@gmail.com>
2020-04-29 15:53:57 -0500

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,41 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(emp_params)
+    if @employee.save
+      redirect_to employee_path(@employee)
+    else
+      redirect_to new_employee_path(@employee)
+    end
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    @employee.update(emp_params)
+    @employee.save
+    redirect_to employee_path(@employee)
+  end
+
+  private
+
+  def emp_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+  end
+
 end
