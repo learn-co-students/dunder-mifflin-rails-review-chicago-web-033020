f625572d25b584d51115e2fb68f0fbd85db5dd37
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-04-29 22:19:32 -0500

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,15 @@
-class Employee < ApplicationRecord
+class Employee < ActiveRecord::Base
+    belongs_to :dog
+
+    def dog_name=(name)
+        self.dog = Dog.find_by(name: name)
+    end
+
+    def dog_name
+        self.dog ? self.dog.name : nil
+    end
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
 end
